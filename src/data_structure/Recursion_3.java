2539df92e2efd82128e46b1ba4f03f205b777944
prannav-ontech <prannavontech@gmail.com>
2025-09-03 23:44:45 +0530

    file

--- src/data_structure/Recursion_3.java
+++ src/data_structure/Recursion_3.java
@@ -1,6 +1,21 @@
 package data_structure;
 
-public class Recursion_3
+public class Recursion_3  // print an element N times using recursion................
 {
+    public static void main(String ar[])
+    {
+      Recursion_3 obj = new Recursion_3();
+      obj.fun(1,5); //Starts from 1 and n is how many times we wanna print n ;
+    }
+ int fun(int i , int n )
+ {
+     if (i>n) return 0;
+
+     else {
+         System.out.println(n);
+         fun(i+1,n);
+     }
+     return 0;
+ }
 
 }
