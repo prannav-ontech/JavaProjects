62ddca929953854583ebc93a46d3e533ee398589
prannav-ontech <prannavontech@gmail.com>
2025-09-07 15:51:29 +0530

    Merge remote-tracking branch 'origin/ds' into ds

---
+++ src/data_structure/Multi_recCall.java
@@ -0,0 +1,21 @@
+package data_structure; // Fibonacci series using recursion.......
+
+
+
+public class Multi_recCall {
+    public static void main(String args [])
+    {
+        Multi_recCall mr=new Multi_recCall();
+        System.out.println(fibo(4));
+    }
+
+    static int fibo(int n)
+    {
+        if (n<=1) return n;
+        else {
+            int last=fibo(n-1);
+            int slast=fibo(n-2);
+            return last+slast;
+        }
+    }
+}
